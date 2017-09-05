81f9baccb1484fe951a48cfcb8ef2b653050c6d2
cezarsantanna <cezar.santanna@gmail.com>
2017-09-05 20:39:59 -0300

    Estrutura básica de diretórios e módulos

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,46 @@
+import importlib
+import os
+import pkgutil
+
 from flask import Flask
+from flask import g
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
-
 import jinja2
 
 class MyApp(Flask):
-    def __init__(self):
+    def __init__(self, implementation):
         Flask.__init__(self, __name__)
         self.jinja_loader = jinja2.ChoiceLoader([
             self.jinja_loader,
             jinja2.PrefixLoader({}, delimiter = ".")
         ])
+        def import_dir(path, prefix):
+            for _, packege, _ in pkgutil.walk_packages([path]):
+                if package[:4] == 'mod_' or package == implementation:
+                    for _, module, _ in pkgutil.iter_modules([path + package]):
+                        if module == 'controller':
+                            controller = importlib.import_module(prefix + '.' + package + '.' + module)
+                            if hasattr(controller, 'mod'):
+                                self.register_blueprint(controller.mod)
+                                print('Registering:', prefix + '.' + package + '.' + module)
+        path = os.path.dirname(__file__) + '/'
+        import_dir(path, 'app')
+        import_dir(path + implementation + '/', 'app.' + implementation)
+
+        @self.errorhandler(404)
+        def not_found(error):
+            return render_template('404.html'), 404
+
+        @self.teardown_appcontext
+        def close_db(error):
+            if hasattr(g, 'cursor'):
+                g.cursor.close()
+            if hasattr(g, 'database'):
+                g.database.close()
+            if hasattr(g, 'clientsDB'):
+                g.clientsDB.close()
+
     def create_global_jinja_loader(self):
         return self.jinja_loader
 
@@ -24,13 +54,3 @@ app = MyApp()
 app.config.from_object('config')
 db = SQLAlchemy(app)
 db.create_all()
-
-# Import a module / component using its blueprint handler variable
-from app.mod_auth.controllers import mod_auth as auth_module
-
-# Register blueprints
-app.register_blueprint(auth_module)
-
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'), 440
---
+++ app/mod_ap/controller.py
@@ -0,0 +1,8 @@
+
+from flask import Blueprint
+from flask import render_template
+mod = Blueprint('ap', __name__, url_prefix = '/ap', template_folder = "templates", static_folder = "static")
+
+@mod.route('/')
+def index():
+    return render_template("ap.index.html")
